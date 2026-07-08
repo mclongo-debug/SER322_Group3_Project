ca042d475b39390564224002440ef4417ec9a0eb
Mia Longo <mclongo@asu.edu>
2026-07-08 00:48:30 -0400

    Integrate cliente menu with database

--- src/main/java/coffee/Menu.java
+++ src/main/java/coffee/Menu.java
@@ -30,7 +30,7 @@ public class Menu {
 
             switch (choice) {
                 case "1":
-                    handleClienteMenu();
+                    clienteMenu();
                     break;
                 case "2":
                     drinkMenu();
@@ -125,6 +125,50 @@ public class Menu {
 
                  default:
                     System.out.println("Invalid choice.");
+            }
+        }
+    }
+
+    private void clienteMenu() {
+
+        Cliente cliente = new Cliente(connection);
+
+        boolean running = true;
+
+        while (running) {
+
+            System.out.println("\n==== Cliente Menu ====");
+            System.out.println("1. Add Cliente");
+            System.out.println("2. Update Cliente");
+            System.out.println("3. Delete Cliente");
+            System.out.println("4. Search Cliente");
+            System.out.println("5. List Clientes");
+            System.out.println("6. Back");
+            System.out.print("Enter choice: ");
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    cliente.addCliente();
+                    break;
+                case "2":
+                    cliente.updateCliente();
+                    break;
+                case "3":
+                    cliente.deleteCliente();
+                    break;
+                case "4":
+                    cliente.searchCliente();
+                    break;
+                case "5":
+                    cliente.listClientes();
+                    break;
+                case "6":
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid option.");
             }
         }
     }
